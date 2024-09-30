fe2840671eb7cb67c9dfe1dd76e33b06e111a084
Herbert <herbert.cardozo@gmail.com>
2024-09-29 22:29:45 -0300

    add docker-compose + correção seeder

--- src/database/seeders/locais.js
+++ src/database/seeders/locais.js
@@ -8,19 +8,19 @@ module.exports = {
         nome: 'Trilha Morro das Aranhas',
         descricao: 'Trilha de aproximadamente 45 min de subida, com uma vista para as praias do Santinho, Moçambique e Ingleses',
         cep: '88058-700',
-        usuarioId: 5
+        usuarioId: 1
       },
       {
         nome: 'Trilha Gravata',
         descricao: 'Trilha fácil',
         cep: '88061-700',
-        usuarioId: 4
+        usuarioId: 2
       },
       {
         nome: 'Trilha Para Galheta',
         descricao: 'Trilha média, com uma vista linda',
         cep: '88061-423',
-        usuarioId: 5
+        usuarioId: 3
       },
       {
         nome: 'Trilha Morro das Feiticeiras',
