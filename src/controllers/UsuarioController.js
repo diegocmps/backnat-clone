25db681c64627fd3ef27c4ecb1cc98f61d3c4a8c
Taline Araujo <taline.araujo@hotmail.com>
2024-10-04 16:07:49 -0300

    Criei a rota de editar usuario, separei a rota usuario em controller, fiz alterações na migrations, inclusão dos dados do endereço e corrigi os seeders

---
+++ src/controllers/UsuarioController.js
@@ -0,0 +1,261 @@
+const Usuario = require('../models/Usuario');
+const axios = require('axios');
+const Local = require('../models/Local');
+const { Op } = require('sequelize');
+
+function validarCPF(cpf) {
+    cpf = cpf.replace(/\D/g, ''); // Remove caracteres especiais
+    if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)) {
+        return false; // CPF deve ter 11 dígitos e não pode ser sequencial
+    }
+    return true;
+}
+
+class UsuarioController {
+    async cadastrar(req, res) {
+        /*
+             #swagger.tags = ['Usuario'],
+             #swagger.parameters = ['body'] ={
+               in: 'body',
+               description: 'Cadastra novos usuários!',
+               schema: {
+                $nome: 'Taline Araujo',
+                $email: 'taline.araujo@hotmail.com',
+                $cpf: '02602502789',
+                sexo: 'Feminino',
+                $senha: 'teste123',
+                $data_nascimento: '1996-04-03',
+                $endereco: 'Vargem pequena',
+                $cep: '12345-678',
+                $rua: 'Rua Exemplo',
+                $numero: '123',
+                $complemento: 'Apto 1',
+                $bairro: 'Bairro Exemplo',
+                $cidade: 'Cidade Exemplo',
+                $estado: 'Estado Exemplo'
+            }   
+        }
+        */ 
+        try {
+            const {
+                nome,
+                email,
+                cpf,
+                sexo,
+                senha,
+                data_nascimento,
+                cep,
+                rua,
+                numero,
+                complemento,
+                bairro,
+                cidade,
+                estado
+            } = req.body;
+    
+            if (!nome) {
+                return res.status(400).json({ message: 'O nome é obrigatório' });
+            }
+    
+            if (!senha) {
+                return res.status(400).json({ message: 'A senha é obrigatória' });
+            }
+    
+            if (!email) {
+                return res.status(400).json({ message: 'O email é obrigatório' });
+            }
+    
+            const usuarioCadastrado = await Usuario.findOne({ where: { email } });
+            if (usuarioCadastrado) {
+                return res.status(400).json({ error: true, message: 'Email já cadastrado!' });
+            }
+    
+            if (!cpf || !validarCPF(cpf)) {
+                return res.status(400).json({ message: 'CPF inválido' });
+            }
+    
+            if (!data_nascimento) {
+                return res.status(400).json({ message: 'A data de nascimento é obrigatória' });
+            }
+    
+            if (!data_nascimento.match(/\d{4}-\d{2}-\d{2}/)) {
+                return res.status(400).json({ message: 'A data de nascimento não está no formato correto' });
+            }
+    
+            const usuario = await Usuario.create({
+                nome,
+                email,
+                cpf,
+                sexo,
+                senha,
+                data_nascimento,
+                cep,
+                rua,
+                numero,
+                complemento,
+                bairro,
+                cidade,
+                estado
+            });
+    
+            res.status(201).json(usuario);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).json({ error: 'Não foi possível cadastrar o usuário' });
+        }
+    }
+
+    async buscarPorId(req, res) {
+        /*
+          #swagger.tags = ['Usuario'],  
+          #swagger.parameters['ID'] = {
+              in: 'query',
+              description: 'Filtrar usuario pelo ID',
+              type: 'string'
+          }
+        */
+        try {
+            const { id } = req.params;
+            const usuario = await Usuario.findByPk(id);
+            if (!usuario) {
+                return res.status(404).json({ error: 'Usuário não encontrado!' });
+            }
+
+            res.json(usuario);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).json({ error: "Erro ao buscar usuário" });
+        }
+    }
+
+    async buscarTodos(req, res) {
+        /*   #swagger.tags = ['Usuario'],  
+             #swagger.parameters['Usuario'] = {
+                 in: 'query',
+                 description: 'Buscar todos os usuarios',
+                 type: 'string'
+         } 
+        */
+        try {
+            const usuarios = await Usuario.findAll();
+            res.json(usuarios);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).json({ error: 'Erro ao buscar usuários' });
+        }
+    }
+
+    async excluir(req, res) {
+        /*  #swagger.tags = ['Usuario'],  
+            #swagger.parameters['Usuario_id'] = {
+                in: 'query',
+                description: 'Excluir usuario',
+                type: 'string'
+        } 
+        */
+        try {
+            const { id } = req.params;
+            const enderecoUsuario = await Local.findOne({ where: { usuarioId: id } });
+
+            if (enderecoUsuario) {
+                return res.status(400).json({ error: true, message: 'Este usuário não pode ser excluído pois possui endereços cadastrados.' });
+            }
+
+            const usuarioExcluido = await Usuario.destroy({ where: { id } });
+
+            if (!usuarioExcluido) {
+                return res.status(404).json({ error: true, message: 'Usuário não encontrado.' });
+            }
+
+            res.json({ message: 'Usuário excluído com sucesso.' });
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).json({ error: 'Não foi possível excluir o usuário.' });
+        }
+    }
+
+    async editar(req, res) {
+        /*
+             #swagger.tags = ['Usuario'],
+             #swagger.parameters = ['body'] ={
+               in: 'body',
+               description: 'Edita um usuário existente!',
+               schema: {
+            $nome: 'Diego Campos',
+            $email: 'diego.campos@hotmail.com',
+            $sexo: 'Masculino',
+            $senha: 'funcionando',
+            $data_nascimento: '1996-05-02',
+            $cep: '12345-678',
+            $rua: 'Rua Atualizada',
+            $numero: '456',
+            $complemento: 'Apto 2',
+            $bairro: 'Bairro Atualizado',
+            $cidade: 'Cidade Atualizada',
+            $estado: 'Estado Atualizado'
+            }   
+        }
+        */ 
+        try {
+            const { id } = req.params; 
+            const {
+                nome,
+                email,
+                sexo,
+                senha,
+                data_nascimento,
+                cep,
+                rua,
+                numero,
+                complemento,
+                bairro,
+                cidade,
+                estado
+            } = req.body;
+
+           
+            const usuario = await Usuario.findByPk(id);
+            if (!usuario) {
+                return res.status(404).json({ message: 'Usuário não encontrado!' });
+            }
+
+            
+            if (!nome) {
+                return res.status(400).json({ message: 'O nome é obrigatório' });
+            }
+
+            if (!email) {
+                return res.status(400).json({ message: 'O email é obrigatório' });
+            }
+
+            
+            const usuarioCadastrado = await Usuario.findOne({ where: { email, id: { [Op.ne]: id } } });
+            if (usuarioCadastrado) {
+                return res.status(400).json({ error: true, message: 'Email já cadastrado!' });
+            }
+
+            
+            await usuario.update({
+                nome,
+                email,
+                sexo,
+                senha, 
+                data_nascimento,
+                cep,
+                rua,
+                numero,
+                complemento,
+                bairro,
+                cidade,
+                estado
+            });
+
+            res.json({ message: 'Usuário atualizado com sucesso!', usuario });
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).json({ error: 'Não foi possível atualizar o usuário' });
+        }
+    }
+}
+
+module.exports = new UsuarioController();
--- src/database/seeders/usuarios.js
+++ src/database/seeders/usuarios.js
@@ -1,58 +1,82 @@
 const { QueryInterface, Sequelize } = require("sequelize");
 const Usuario = require('../../models/Usuario');
 
-module.exports ={
+module.exports = {
     up: async (QueryInterface, Sequelize) => {
         await Usuario.bulkCreate([
             {
-                nome:"Raphaela Assis",
-                email:"rapha.exemplo@hotmail.com",
+                nome: "Raphaela Assis",
+                email: "rapha.exemplo@hotmail.com",
                 cpf: "01236523879",
-                sexo:"Feminino",
-                senha:"rapha123",
-                data_nascimento:"1989-01-05",
-                endereco:"Campeche"
+                sexo: "Feminino",
+                senha: "rapha123",  
+                data_nascimento: "1989-01-05",
+                cep: "88130-000",
+                rua: "Rua A",
+                numero: 123,
+                complemento: "Apto 1",
+                bairro: "Campeche",
+                cidade: "Florianópolis",
+                estado: "SC"
             },
             {
-                nome:"kauana Araujo",
-                email:"kaka.exemplo@hotmail.com",
+                nome: "Kauana Araujo",
+                email: "kaka.exemplo@hotmail.com",
                 cpf: "14151917987",
-                sexo:"Feminino",
-                senha:"kaka123",
-                data_nascimento:"2002-12-09",
-                endereco:"Ratones"
+                sexo: "Feminino",
+                senha: "kaka123",  
+                data_nascimento: "2002-12-09",
+                cep: "88060-000",
+                rua: "Rua B",
+                numero: 456,
+                complemento: "Casa",
+                bairro: "Ratones",
+                cidade: "Florianópolis",
+                estado: "SC"
             },
             {
-                nome:"Tiago Araujo",
-                email:"tiago.exemplo@hotmail.com",
+                nome: "Tiago Araujo",
+                email: "tiago.exemplo@hotmail.com",
                 cpf: "78945632115",
-                sexo:"masculino",
-                senha:"tiago123",
-                data_nascimento:"1992-05-10",
-                endereco:"Arambaré"
+                sexo: "Masculino",
+                senha: "tiago123",  
+                data_nascimento: "1992-05-10",
+                cep: "88120-000",
+                rua: "Rua C",
+                numero: 789,
+                complemento: "",
+                bairro: "Arambaré",
+                cidade: "Florianópolis",
+                estado: "SC"
             },
             {
-                nome:"Maria Eduarda",
-                email:"duda.exemplo@hotmail.com",
-                cpf: "526874136985",
-                sexo:"Feminino",
-                senha:"duda123",
-                data_nascimento:"2002-09-19",
-                endereco:"Camaquã"
+                nome: "Maria Eduarda",
+                email: "duda.exemplo@hotmail.com",
+                cpf: "52687413698",
+                sexo: "Feminino",
+                senha: "duda123", 
+                data_nascimento: "2002-09-19",
+                cep: "88110-000",
+                rua: "Rua D",
+                numero: 321,
+                complemento: "Bloco B",
+                bairro: "Camaquã",
+                cidade: "Florianópolis",
+                estado: "SC"
             }
-            
-        ])
-
+        ]);
     },
 
-   down: async (QueryInterface, Sequelize) => {
-    await Usuario.destroy({email:[
-        "rapha.exemplo@hotmail.com",
-        "kaka.exemplo@hotmail.com",
-        "tiago.exemplo@hotmail.com",
-        "duda.exemplo@hotmail.com"
-        ]
-    })
-   }
-    
-}
+    down: async (QueryInterface, Sequelize) => {
+        await Usuario.destroy({
+            where: {
+                email: [
+                    "rapha.exemplo@hotmail.com",
+                    "kaka.exemplo@hotmail.com",
+                    "tiago.exemplo@hotmail.com",
+                    "duda.exemplo@hotmail.com"
+                ]
+            }
+        });
+    }
+};
