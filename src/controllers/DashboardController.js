619f4d63fc7aa949b4ac1c602a2bd25b2681bf2c
Herbert <herbert.cardozo@gmail.com>
2024-10-07 23:13:43 -0300

    implementação de paginação

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -1,3 +1,4 @@
+const PagedResult = require("../classes/PagedResult");
 const Local = require("../models/Local");
 const Usuario = require("../models/Usuario");
 
@@ -7,6 +8,7 @@ class DashboardController {
 
         try {
 
+            const { page = 1, limit = 10 } = req.query;
 
             const usuariosTotal = await Usuario.count()
             const usuariosOnline = await Usuario.count({ where: { isLogged: true } })
@@ -16,12 +18,13 @@ class DashboardController {
                     model: Usuario,
                     as: "usuario",
                     attributes: ['nome']
-
-
-                }
+                },
+                order: [['id', 'ASC']],
+                limit: limit,
+                offset: (page - 1) * limit
             })
 
-            const data = {
+            const dados = {
                 usuariosTotal: usuariosTotal,
                 usuariosOnline: usuariosOnline,
                 locaisTotal: locaisTotal,
@@ -29,7 +32,7 @@ class DashboardController {
 
             }
 
-            return res.status(200).json({ ...data })
+            return res.status(200).json(new PagedResult(page, limit, dados, locaisTotal))
         }
         catch (error) {
             return res.status(500).json({ message: "Erro ao recuperar dados" })
