9ba0a9b8e596995ab66893513305e5247862ace1
Herbert <herbert.cardozo@gmail.com>
2024-10-11 16:57:14 -0300

    mudança no nome das variaves de ambiente da database

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -2,10 +2,22 @@ const { config } = require('dotenv')
 config()
 
 module.exports = {
-  dialect: process.env.DIALECT, //Qual banco de dados está utilizando;
-  host: process.env.HOST, //Qual servidor está utilizando;
-  username: process.env.USERNAMEDB, //Qual o nome do seu usuário no postgres;
-  password: process.env.PASSWORDDB, //Qual a senha do seu usuário no postgres;
+  dialect: process.env.DB_DIALECT, //Qual banco de dados está utilizando;
+  host: process.env.DB_HOST, //Qual servidor está utilizando;
+  username: process.env.DB_USERNAME, //Qual o nome do seu usuário no postgres;
+  password: process.env.DB_PASSWORD, //Qual a senha do seu usuário no postgres;
   database: process.env.DATABASE, //Qual o nome do seu database no postgres;
-  port: process.env.PORT //Qual porta do seu postgres (Normalmente é a 5432);
+  port: process.env.DB_PORT, //Qual porta do seu postgres (Normalmente é a 5432);
+  // dialectOptions: {
+  //   ssl: {
+  //     rejectUnauthorized: true,
+  //     require: true
+  //   }
+  // },
+  // pool: {
+  //   max: 5,
+  //   min: 0,
+  //   acquire: 30000,
+  //   idle: 10000 // idle timeout in milliseconds
+  // }
 };
